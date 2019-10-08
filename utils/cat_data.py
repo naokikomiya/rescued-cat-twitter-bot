48f1580df9b668b528aba0edef67d34b1f299be6
Naoki Komiya <edsa9654@gmail.com>
2019-10-08 09:58:19 +0900

    fix driver-path

--- utils/cat_data.py
+++ utils/cat_data.py
@@ -12,10 +12,8 @@ def get():
     url = 'https://tokyocatguardian.org/cats_date/'
     wait_time = 5
 
-    # driver_path = '/app/.chromedriver/bin/chromedriver'
-    driver_path = os.environ.get('GOOGLE_CHROME_BIN')
-    print(os.environ.get('GOOGLE_CHROME_BIN'))
-    print(os.environ.get('GOOGLE_CHROME_SHIM'))
+    # heroku上のdriverパス
+    driver_path = '/app/.chromedriver/bin/chromedriver'
     options = web_driver.option()
     driver = webdriver.Chrome(options=options, executable_path=driver_path)
     driver.get(url)
